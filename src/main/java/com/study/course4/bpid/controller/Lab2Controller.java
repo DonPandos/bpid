e7c8ea15d54b9963f5279ed6fdfac2a62b871448
Bogdan <ewrr_96@mail.ru>
2020-09-20 14:04:32 +0300

    lab2 started

---
+++ src/main/java/com/study/course4/bpid/controller/Lab2Controller.java
@@ -0,0 +1,10 @@
+package com.study.course4.bpid.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lab2")
+public class Lab2Controller {
+
+}
---
+++ src/main/java/com/study/course4/bpid/crypt/PermutationCrypt.java
@@ -0,0 +1,13 @@
+package com.study.course4.bpid.crypt;
+
+import javafx.util.Pair;
+
+public class PermutationCrypt {
+
+    public Pair<String, String> encode(String str){
+        String key;
+        String encodeString;
+
+        return new Pair<>(encodeString, key);
+    }
+}
