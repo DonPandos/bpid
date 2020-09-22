5c57ed60711c2200e39840a0d2843d1dd0f74b2c
Bogdan <ewrr_96@mail.ru>
2020-09-22 21:17:11 +0300

    Encode processing

--- src/main/java/com/study/course4/bpid/crypt/PermutationCrypt.java
+++ src/main/java/com/study/course4/bpid/crypt/PermutationCrypt.java
@@ -2,12 +2,48 @@ package com.study.course4.bpid.crypt;
 
 import javafx.util.Pair;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class PermutationCrypt {
 
-    public Pair<String, String> encode(String str){
+    public static Pair<String, String> encode(String str){
         String key;
         String encodeString;
+        Integer arrWidth = (int)Math.ceil((double)str.length()/5);
+        Character[][] arr = new Character[5][arrWidth]; // инициализируем массив высотой 5, длиной, которой хватит для вмещения символов
+        int counter = 0;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < arrWidth; j++){
+                System.out.println(i + ", " + j);
+                try {
+                    arr[i][j] = str.charAt(counter) != ' ' ? str.charAt(counter) : '_';
+                } catch (StringIndexOutOfBoundsException e){
+                    arr[i][j] = '_';
+                } finally {
+                    counter++;
+                }
+            }
+        } // заполнение массива строкой
 
-        return new Pair<>(encodeString, key);
+        Integer iterationsCount = (int)Math.ceil((double)arrWidth / 10); // количество итераций
+
+
+        //tests
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < arrWidth; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        //
+        //return new Pair<>(encodeString, key);
+        return null;
+    }
+
+    public static void main(String[] args) {
+        encode("Hello worldHello world world!HellHello world world!o world world!Hello world world!Hello world world! world!");
+
     }
 }
