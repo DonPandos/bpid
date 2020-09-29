b6ebf3f0f509b3815250bdc3bf60d813327a7168
Bogdan <ewrr_96@mail.ru>
2020-09-29 12:27:48 +0300

    PermutaionCrypt done

--- src/main/java/com/study/course4/bpid/crypt/PermutationCrypt.java
+++ src/main/java/com/study/course4/bpid/crypt/PermutationCrypt.java
@@ -12,8 +12,8 @@ public class PermutationCrypt {
         Integer arrWidth = (int)Math.ceil((double)str.length()/5);
         Character[][] arr = new Character[arrWidth][5]; // инициализируем массив высотой 5, длиной, которой хватит для вмещения символов
         int counter = 0;
-        for(int i = 0; i < arrWidth; i++){
-            for(int j = 0; j < 5; j++){
+        for(int j = 0; j < 5; j++){
+          for(int i = 0; i < arrWidth; i++){
                 System.out.println(i + ", " + j);
                 try {
                     arr[i][j] = str.charAt(counter) != ' ' ? str.charAt(counter) : '_';
@@ -56,36 +56,33 @@ public class PermutationCrypt {
     }
 
     public static String decode(Pair<String, String> keyValue){
-        String key = keyValue.getKey();
-        String value = keyValue.getValue();
-        Character[][] arr = new Character[key.length()][5];
-        int iterator = 0;
-        int keyIterations = (int)Math.ceil((double)key.length() / 10);
-        System.out.println("keyIt: " + keyIterations);
-        while(iterator < keyIterations){
-            Integer currKey = Integer.parseInt(String.valueOf(key.charAt(0)));
-            for(int j = 0; j < 5; j++){
-                System.out.println("val: " + (iterator * 50) + (currKey * 5) + j);
-                arr[currKey][j] = value.charAt((iterator * 50) + (currKey + 5) + j);
-            }
-            iterator++;
-        }
-//        System.out.println(Arrays.deepToString(arr));
         String result = "";
-        for(int j = 0; j < key.length(); j++){
-            for(int i = 0; i < 5; i++){
-//                System.out.println(String.valueOf(arr[i][j]));
-                result += String.valueOf(arr[i][j]);
+        String key = keyValue.getKey(); // ключ
+        String encodeString = keyValue.getValue(); // зашифрованная фраза
+        Character[][] arr = new Character[key.length()][5]; // массив для расшифровки
+        Integer iterationsCount = (int) Math.ceil((double)key.length() / 10); // количество итерации ключа
+        System.out.println(iterationsCount);
+        Integer currentIteration = 0; // текущая итерация ключа
+
+        while(currentIteration < iterationsCount){ // пока текущая итерация не последняя
+            Integer currentIterationKeyLength = key.length() > 9 ? 10 : key.length(); // количество столбцов в текущей итерации ключа(всегда 10, кроме последней итерации)
+            for(int i = 0; i < currentIterationKeyLength; i++){
+                Integer currentKey = Integer.parseInt(String.valueOf(key.charAt(0))); // текущий ключ в данной итерации
+                Integer currentColumn = (currentIteration * 10) + currentKey; // номер столбца
+                key = key.substring(1, key.length());
+                for(int j = 0; j < 5; j++){
+                    arr[currentColumn][j] = encodeString.charAt(0);
+                    encodeString = encodeString.substring(1, encodeString.length());
+                }
+            }
+            currentIteration++;
+        }
+        for(int j = 0; j < 5; j++){
+            for(int i = 0; i < arr.length; i++){
+                result += String.valueOf(arr[i][j]).equals("_") ? " " : String.valueOf(arr[i][j]);
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
-        Pair<String, String> pair = encode("Фантастический американский сериал, снятый по мотивам комиксов Marvel. Агенты организации Щ.И.Т. снова объединяются, спасая Землю от глобальных угроз с помощью невероятных способностей. " +
-                "Действие в новом сериале происходит после событий в блокбастере «Мстители». Выживший агент Фил Колсон собирает группу специалистов для действия в рамках засекреченной организации Щ.И.Т., специализирующейся только на самых сложных, необъяснимых и загадочных происшествиях.");
-        System.out.println(pair.getKey());
-        System.out.println(pair.getValue());
-        System.out.println(decode(new Pair<>("12786305942071645983410369852797815236048217594630380421769568923041754173568290974365812001", "стический__снятый_пориал,америФантаий_се_мотикансков_Maвам_кации_омиксганиз_Агенты_ор._сноЩ.И.Тrvel.сая_Зъединва_об,_спаот_глгроз_ных_уемлю_яютсяобальв_нов_Действие_ощью_собноневероятныстей.с_помх_споокбас_проириале_в_блле_сотере_т_посбытийсходиом_сеившийбирае«Мсти_аген._Выжтели»он_со_Колст_грут_Филв_рамасекреченнлистов_дляппу_с_дейспециаках_зтвия_.,_спганизющейсЩ.И.Тециализируя_толации_ько_ной_ороисшеадочнбъясн,_неои_загимых_ых_прых_сложныха_самствиях.___")));
-    }
 }
