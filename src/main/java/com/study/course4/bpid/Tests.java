193308ee56111c703ec34e8489b3d511dc17a6ba
DonPandos <ewrr_96@mail.ru>
2020-10-12 00:33:42 +0300

    Just save to rep

--- src/main/java/com/study/course4/bpid/Tests.java
+++ src/main/java/com/study/course4/bpid/Tests.java
@@ -9,10 +9,18 @@ import java.util.List;
 
 public class Tests {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String toEncode = "12. Шаньгин";
+        String toEncode = new String("Русский".getBytes("UTF-8"));
+        System.out.println(toEncode.length());
         Pair<String, List<String>> pair = FeistCrypt.encode(new StringBuilder(toEncode), 8);
+        System.out.println("encoded: " + pair.getKey());
         String decodedString = FeistCrypt.decode(pair.getKey(), pair.getValue());
-        System.out.println(decodedString.getBytes("UTF-8"));
+        System.out.println(decodedString);
+//        String toEncode = "StringTOsdasdasdEncode";
+//        String encodedString = Base64Crypt.base64Encode(toEncode.getBytes());
+//        System.out.println("encodedStirng : " + encodedString);
+//        String decodedString = new String(Base64Crypt.base64Decode(encodedString));
+//        System.out.println("decodedString : " + decodedString);
+//
 
 //        String encoded = Base64Crypt.encode("QWERTYUIOP{}ASDFGHJKL:|ZXCVBNM<>?qwertyuiop[]asdfghjkl;zxcvbnm,./йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,", "ASFSDF");
 //        System.out.println(Base64Crypt.decode(encoded, "ASFSDF"));
--- src/main/java/com/study/course4/bpid/crypt/Base64Crypt.java
+++ src/main/java/com/study/course4/bpid/crypt/Base64Crypt.java
@@ -5,45 +5,38 @@ import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 public class Base64Crypt {
 
     public static String encode(String s, String key) {
-        try {
-            return base64Encode(xorWithKey(s.getBytes("UTF-8"), key.getBytes("UTF-8")));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return base64Encode(xorWithKey(s.getBytes(), key.getBytes()));
     }
 
     public static String decode(String s, String key) {
-        try {
-            return new String(xorWithKey(base64Decode(new String(s.getBytes("UTF-8"))), key.getBytes("UTF-8")));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return new String(xorWithKey(base64Decode(s), key.getBytes()));
+
     }
 
     public static byte[] xorWithKey(byte[] a, byte[] key) {
         byte[] out = new byte[a.length];
         for (int i = 0; i < a.length; i++) {
-            out[i] = (byte) (a[i] ^ key[i%key.length]);
+            out[i] = (byte) (a[i] ^ key[i % key.length]);
         }
         return out;
     }
 
-    private static byte[] base64Decode(String s) {
-        try {
-            BASE64Decoder d = new BASE64Decoder();
-            return d.decodeBuffer(s);
-        } catch (IOException e) {throw new RuntimeException(e);}
+    public static byte[] base64Decode(String s) {
+//        try {
+//            BASE64Decoder d = new BASE64Decoder();
+//            return d.decodeBuffer(s);
+//        } catch (IOException e) {throw new RuntimeException(e);}
+        return Base64.getDecoder().decode(s);
     }
 
-    private static String base64Encode(byte[] bytes) {
-        BASE64Encoder enc = new BASE64Encoder();
-        return enc.encode(bytes).replaceAll("\\s", "");
-
+    public static String base64Encode(byte[] bytes) {
+//        BASE64Encoder enc = new BASE64Encoder();
+//        return enc.encode(bytes).replaceAll("\\s", "");
+        return Base64.getEncoder().encodeToString(bytes);
     }
 }
